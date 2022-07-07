f61524778f852b4951bbd06a07846be08e0abd44
anismousse <anismousse@gmail.com>
2022-07-06 22:44:57 -0700

    Adding web frame work and implementing 'health_check' endoint (#1)
    * Add Rocket webframework
    
    * Implement 'health_check' endpoint
    
    * Update health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+use rocket::http::Status;
+
+#[get("/health_check")]
+async fn health_check() -> Status {
+    Status::Ok
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![health_check])
 }

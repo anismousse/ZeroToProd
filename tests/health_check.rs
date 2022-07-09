f9e865cb0779ec52104c9bfdf70eba14a3a76be1
anismousse <anismousse@gmail.com>
2022-07-09 14:30:25 -0700

    Add second integration test utilizing tokio and rocket configuration

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,17 +1,39 @@
-use rocket::local::blocking::Client;
 use rocket::http::Status;
-use zero2prod::startup;
+use rocket::local::blocking::Client;
+use rocket::tokio;
+use zero2prod::{build_rocket_config, startup, startup_default};
 
 #[test]
 fn health_check_rocket_test() {
+    let client = Client::tracked(startup_default()).unwrap();
 
-  let client = Client::tracked(startup()).unwrap();
+    // Test for the health_check endpoint
+    let response = client.get("/health_check").dispatch();
+    assert_eq!(response.status(), Status::Ok);
 
-  // Test for the health_check endpoint
-  let response = client.get("/health_check").dispatch();
-  assert_eq!(response.status(), Status::Ok);
+    // Test for a non existing endpoint
+    let response = client.get("/toto").dispatch();
+    assert_eq!(response.status(), Status::NotFound);
+}
 
-  // Test for a non existing endpoint
-  let response = client.get("/toto").dispatch();
-  assert_eq!(response.status(), Status::NotFound);
+#[tokio::test]
+async fn health_check() {
+    let address_port = spawn_app();
+    let client = reqwest::Client::new();
+
+    let url = format!("{}/health_check", &address_port);
+    let err_msg = "Can not execute the request";
+
+    let response = client.get(url).send().await.expect(err_msg);
+
+    assert!(response.status().is_success());
+    assert_eq!(response.content_length(), Some(0));
+}
+
+fn spawn_app() -> String {
+    // Building configuration object for Rocket
+    let config = build_rocket_config();
+    let server = startup(&config).expect("Failed to bind address");
+    let _ = tokio::spawn(server.launch());
+    format!("http://{}:{}", config.address, &config.port.to_string())
 }

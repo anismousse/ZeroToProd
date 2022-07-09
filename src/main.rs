5584ace277b08580e089cb37f0e4b130edd721d7
anismousse <anismousse@gmail.com>
2022-07-09 14:29:18 -0700

    clean up main file

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
 extern crate rocket;
-use rocket::Config;
-use std::net::TcpListener;
-use zero2prod::startup;
+use zero2prod::{build_rocket_config, startup};
 
 #[rocket::main]
 #[allow(unused_must_use)]
 async fn main() {
-    // Get available port
-    let port = match TcpListener::bind("127.0.0.1:0") {
-        Ok(listener) => listener.local_addr().unwrap().port(),
-        Err(_) => panic!("No port available"),
-    };
-
     // Building configuration object for Rocket
-    let config = Config {
-        port,
-        ..Config::debug_default()
-    };
+    let config = build_rocket_config();
 
     // launch Rocket
-    startup(config).launch().await;
+    startup(&config)
+        .expect("Failed to bind address")
+        .launch()
+        .await;
 }

81994ace8517e36cbb69437b541baeeb42a0f2b3
anismousse <anismousse@gmail.com>
2022-07-09 13:52:11 -0700

    Ulize the new startup method

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 extern crate rocket;
+use rocket::Config;
+use std::net::TcpListener;
 use zero2prod::startup;
 
 #[rocket::main]
 #[allow(unused_must_use)]
 async fn main() {
-    startup().launch().await;
+    // Get available port
+    let port = match TcpListener::bind("127.0.0.1:0") {
+        Ok(listener) => listener.local_addr().unwrap().port(),
+        Err(_) => panic!("No port available"),
+    };
+
+    // Building configuration object for Rocket
+    let config = Config {
+        port,
+        ..Config::debug_default()
+    };
+
+    // launch Rocket
+    startup(config).launch().await;
 }

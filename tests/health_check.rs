63a5d5839eb2b6b0ddafc0af24f0cd9ddda0ed6f
anismousse <anismousse@gmail.com>
2022-07-09 21:19:52 -0700

    Update health check test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -24,8 +24,7 @@ async fn health_check() {
     let url = format!("{}/health_check", &address_port);
     // let err_msg = "Can not execute the request";
 
-    let response = client.get(url).send().await.unwrap();
-    //.expect(err_msg);
+    let response = client.get(url).send().await.expect(err_msg);
 
     assert!(response.status().is_success());
     assert_eq!(response.content_length(), Some(0));

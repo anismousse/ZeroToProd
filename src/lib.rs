b89b3fb17c7d0827c0ffa99185d3c4ba0a5e3f9a
anismousse <anismousse@gmail.com>
2022-07-16 09:58:40 -0700

    Add 'subscriptions' endpoint and associated tests (#5)
    * Add 'subscriptions' endpoint and associated tests
    
    * Add From data processing at the 'subscription' endpoint
    
    * Fix linting issues
    
    * Fix clippy warning

--- src/lib.rs
+++ src/lib.rs
@@ -3,22 +3,50 @@ extern crate rocket;
 
 use std::net::TcpListener;
 
+use regex::Regex;
+use rocket::form::Form;
+use rocket::form::{self, Error};
 use rocket::http::Status;
 use rocket::Config;
 use rocket::{Build, Rocket};
+
+#[derive(FromForm)]
+struct Subscriber<'r> {
+    #[field(validate = omits("no"))]
+    name: &'r str,
+    #[field(validate = omits("no"))]
+    #[field(validate = validate_email())]
+    email: String,
+}
+
+fn validate_email<'v>(email: &str) -> form::Result<'v, ()> {
+    let email_domain_regex = Regex::new(
+        r"^([a-z0-9_+]([a-z0-9_+.]*[a-z0-9_+])?)@([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6})",
+    )
+    .unwrap();
+    if !email_domain_regex.is_match(email) {
+        return Err(Error::validation("invalid email address provided").into());
+    }
+    Ok(())
+}
 
 #[get("/health_check")]
 async fn health_check() -> Status {
     Status::Ok
 }
 
+#[post("/subscriptions", data = "<subscriber>")]
+fn subscriptions(subscriber: Form<Subscriber<'_>>) -> String {
+    format!(" {} - {}", subscriber.name, subscriber.email)
+}
+
 pub fn startup(config: &rocket::Config) -> Result<Rocket<Build>, std::io::Error> {
-    let server = rocket::custom(config).mount("/", routes![health_check]);
+    let server = rocket::custom(config).mount("/", routes![health_check, subscriptions]);
     Ok(server)
 }
 
 pub fn startup_default() -> Rocket<Build> {
-    rocket::build().mount("/", routes![health_check])
+    rocket::build().mount("/", routes![health_check, subscriptions])
 }
 
 pub fn build_rocket_config() -> rocket::Config {
---
+++ tests/subscriptions.rs
@@ -0,0 +1,44 @@
+use std::vec;
+
+use rocket::http::ContentType;
+use rocket::http::Status;
+use rocket::local::blocking::Client;
+use zero2prod::startup_default;
+
+#[test]
+fn test_subscriptions_with_valid_form_data_rocket_test() {
+    let client = Client::tracked(startup_default()).unwrap();
+    let body = "name=Akin%20Mousse&email=anismousse%40gmail.com";
+    let response = client
+        .post("/subscriptions")
+        .header(ContentType::Form)
+        .body(body)
+        .dispatch();
+    assert_eq!(response.status(), Status::Ok);
+}
+
+#[test]
+fn test_subscriptions_with_invalid_form_data_rocket_test() {
+    let client = Client::tracked(startup_default()).unwrap();
+    let test_cases = vec![
+        ("missing email", "name=Akin%20Mousse"),
+        ("missing name", "email=anismousse%40gmail.com"),
+        ("missing name and email", ""),
+        ("an incorrect email", "email=anismousse%40gm%40gmail.com"),
+    ];
+
+    for (err_msg, incorrect_body) in test_cases {
+        let response = client
+            .post("/subscriptions")
+            .header(ContentType::Form)
+            .body(&incorrect_body)
+            .dispatch();
+
+        assert_eq!(
+            response.status(),
+            Status::UnprocessableEntity,
+            "The API did not fail with 422 despite {} in the payload.",
+            err_msg
+        );
+    }
+}

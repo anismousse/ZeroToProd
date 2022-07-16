703d64bda90d296446e1e9b8e26570db19ab5eb8
anismousse <anismousse@gmail.com>
2022-07-16 14:02:29 -0700

    Utilizing the configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 extern crate rocket;
 use zero2prod::{build_rocket_config, startup};
+use zero2prod::configuration::get_configuration;
 
 #[rocket::main]
 #[allow(unused_must_use)]
 async fn main() {
+    let configuration = get_configuration().expect("Fail to load the configuration");
+
     // Building configuration object for Rocket
-    let config = build_rocket_config();
+    let config = build_rocket_config(Some(configuration.application_port));
 
     // launch Rocket
     startup(&config)
